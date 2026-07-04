eb3fb615d4ff4be950ecd11d5b5ea97570aaf9c9
Sehrab Riyaz <darsehraab@gmail.com>
2026-07-04 16:10:59 +0530

    some backend edits

--- src/menu/dto/create-menu.dto.ts
+++ src/menu/dto/create-menu.dto.ts
@@ -1,28 +1,32 @@
 import { IsBoolean, IsEnum, IsNotEmpty, IsNumber, IsOptional, IsString, IsUrl, MaxLength, Min } from "class-validator";
 
 import { Category } from "../../core/global.constraints";
+import { ApiProperty } from "@nestjs/swagger";
 
 export class CreateMenuDto {
   
   @IsString()
   @IsNotEmpty()
-  @MaxLength(256)
+  @ApiProperty({ maxLength: 256, example: 'Red Velvet Cake' })
   name: string;
 
   @IsString()
   @IsOptional()
-  @MaxLength(500)
+  @ApiProperty({ maxLength: 500, example: 'Aromatic desert with rose flavoured crust' })
   description?: string;
 
   @IsNumber()
   @Min(0)
+  @ApiProperty({ example: '5.99' })
   price: number;
 
   @IsEnum(Category)
+  @ApiProperty({ maxLength: 256, example: "starter" })
   category: Category;
 
   @IsBoolean()
   @IsOptional()
+  @ApiProperty({ example: true })
   available?: boolean;
 
   @IsUrl()
--- src/menu/dto/menu-response.dto.ts
+++ src/menu/dto/menu-response.dto.ts
@@ -3,28 +3,35 @@ import { IsBoolean, IsEnum, IsNumber, IsString, IsUrl } from 'class-validator';
 
 import { BaseDto } from '../../core/dto/base.dto';
 import { Category } from '../../core/global.constraints';
+import { Expose } from 'class-transformer';
 
 export class MenuResponseDto extends BaseDto {
+  @Expose()
   @IsString()
   @ApiProperty({ example: 'Brisket 007' })
   name: string;
 
+  @Expose()
   @IsString()
   @ApiProperty({ example: 'Medium Rare Steak' })
   description?: string;
 
+  @Expose()
   @IsNumber()
   @ApiProperty({ example: '3.99' })
   price: number;
 
+  @Expose()
   @IsEnum(Category)
   @ApiProperty({ example: 'Non-veg/Veg' })
   category: Category;
 
+  @Expose()
   @IsBoolean()
   @ApiProperty({ example: 'true/false' })
   available?: boolean;
 
+  @Expose()
   @IsUrl()
   @ApiProperty({ example: 'uploads/img1.jpg' })
   imageUrl?: string;
